ba8d24ad3c2b440dfe5c4c61ab228cdc4c93e79f
DustWu <154425443+DustWu@users.noreply.github.com>
2024-01-12 01:11:16 +0800

    用户端的debug尝试

---
+++ src/main/java/cn/zjut/controller/phoneController.java
@@ -0,0 +1,27 @@
+package cn.zjut.controller;
+import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
+import cn.zjut.common.R;
+import cn.zjut.entity.Product;
+import cn.zjut.service.ProductService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.BeanUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+@CrossOrigin
+@RestController
+@RequestMapping("/api")
+public class phoneController {
+
+
+
+    @Autowired
+    private ProductService productService; // 你的产品服务类，类似于你之前的Product模块
+
+    @GetMapping("/{productId}")
+    public R<Product> get(@PathVariable Long productId ) {
+        Product product = productService.getByIdWithFlavor(productId);
+        return R.success(product);
+    }
+}
+
