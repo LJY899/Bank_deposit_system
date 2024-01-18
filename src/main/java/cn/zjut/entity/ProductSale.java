12908c0faff00f66a8f4ffcc76300d1f4ca590ff
LiuJY <127284382+LJY899@users.noreply.github.com>
2024-01-18 17:22:02 +0800

    修改数据统计实体，添加产品名称属性

--- src/main/java/cn/zjut/entity/ProductSale.java
+++ src/main/java/cn/zjut/entity/ProductSale.java
@@ -1,5 +1,6 @@
 package cn.zjut.entity;
 
+import com.baomidou.mybatisplus.annotation.TableField;
 import lombok.Data;
 
 import java.math.BigDecimal;
@@ -7,6 +8,8 @@ import java.math.BigDecimal;
 @Data
 public class ProductSale {
     private Integer productId;
+    @TableField("name")
+    private String productName;
     private Integer totalSale; // Ensure this matches the database and query
     private Double totalAmount;
 }
